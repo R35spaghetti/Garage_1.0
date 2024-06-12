903c86515017af210374affcab607baf3d14e82d
RB <rkrb@tyfonmail.se>
2024-06-12 11:45:30 +0200

    Car added

---
+++ Garage_1.0/Models/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace Garage_1._0.Models;
+
+public class Car : Vehicle
+{
+    private string EngineLayout { get; set; }
+    
+    public Car(string engineLayout)
+    {
+        EngineLayout = engineLayout;
+    }
+
+}
