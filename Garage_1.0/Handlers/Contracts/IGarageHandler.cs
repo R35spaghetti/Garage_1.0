41ffc4849c8d5fd9a7b7307af2bb60ae022a7eb5
RB <rkrb@tyfonmail.se>
2024-06-12 12:27:29 +0200

    IGarageHandler contract added

---
+++ Garage_1.0/Handlers/Contracts/IGarageHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Garage_1._0.Handlers.Contracts;
+
+public interface IGarageHandler
+{
+    void ShowAllVehicles();
+    void ShowVehicleTypes();
+    void RemoveVehicle();
+    int SetGarageCapacity();
+    void PopulateGarage();
+    void FindNumberPlate();
+    void FindVehicle();
+}
---
+++ Garage_1.0/Handlers/GarageHandler.cs
@@ -0,0 +1,41 @@
+﻿using Garage_1._0.Handlers.Contracts;
+
+namespace Garage_1._0.Handlers;
+
+public class GarageHandler : IGarageHandler
+{
+    public void ShowAllVehicles()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ShowVehicleTypes()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveVehicle()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int SetGarageCapacity()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void PopulateGarage()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void FindNumberPlate()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void FindVehicle()
+    {
+        throw new NotImplementedException();
+    }
+}
