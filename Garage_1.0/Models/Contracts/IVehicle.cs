a6f61545d74ea71e883e924d166af2161363e455
RB <rkrb@tyfonmail.se>
2024-06-13 14:09:04 +0200

    Vehicle can be nullable

--- Garage_1.0/Models/Contracts/IVehicle.cs
+++ Garage_1.0/Models/Contracts/IVehicle.cs
@@ -2,9 +2,9 @@
 
 public interface IVehicle
 {
-    string NumberPlate { get; set; }
-    string Colour { get; set; }
-    string FuelType { get; set; }
+    string? NumberPlate { get; set; }
+    string? Colour { get; set; }
+    string? FuelType { get; set; }
     int Wheels { get; set; }
     int Year { get; set; } 
     string ToString();
--- Garage_1.0/Models/Vehicle.cs
+++ Garage_1.0/Models/Vehicle.cs
@@ -4,9 +4,9 @@ namespace Garage_1._0.Models;
 
 public class Vehicle : IVehicle
 {
-    public required string NumberPlate { get; set; }
-    public string Colour { get; set; }
-    public string FuelType { get; set; }
+    public required string? NumberPlate { get; set; }
+    public string? Colour { get; set; }
+    public string? FuelType { get; set; }
     public int Wheels { get; set; }
     public int Year { get; set; }
 
