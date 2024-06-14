03cf8db6f3472c9f692899d37da26bb4c493ba33
RB <rkrb@tyfonmail.se>
2024-06-14 09:15:10 +0200

    Added Motorcycle.cs

---
+++ Garage_1.0/Models/Motorcycle.cs
@@ -0,0 +1,21 @@
+﻿namespace Garage_1._0.Models;
+
+public class Motorcycle : Vehicle
+{
+    private int Length { get; set; }
+
+    public Motorcycle(int length)
+    {
+        Length = length;
+    }
+
+    public override string ToString()
+    {
+        return $" Numberplate: {NumberPlate}\n" +
+               $"Colour: {Colour}\n" +
+               $"Fuel type: {FuelType}\n" +
+               $"Wheels: {Wheels}\n" +
+               $"Year: {Year}\n" +
+               $"Length: {Length}";
+    }
+}
