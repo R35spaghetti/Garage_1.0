314fbd735715a59d4b8c3e05e68fe5daaecdfbae
RB <rkrb@tyfonmail.se>
2024-06-14 13:11:37 +0200

    UserInput.cs handles enum

--- Garage_1.0/UserInteraction/UserInput.cs
+++ Garage_1.0/UserInteraction/UserInput.cs
@@ -8,4 +8,13 @@ public static class UserInput
         var input = Console.ReadLine() ?? string.Empty;
         return (T)Convert.ChangeType(input, typeof(T));
     }
+
+    public static T GetInputEnum<T>() where T : struct, Enum
+    {
+        Console.WriteLine("Enter value: ");
+        var input = Console.ReadLine() ?? string.Empty;
+        Enum.TryParse(input, out T result);
+
+        return result;
+    }
 }
