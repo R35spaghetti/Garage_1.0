f5abf0d051d5267ee029f05dcd968a440bb2591b
RB <rkrb@tyfonmail.se>
2024-06-14 11:00:43 +0200

    generic method for user input

---
+++ Garage_1.0/UserInteraction/UserInput.cs
@@ -0,0 +1,11 @@
+﻿namespace Garage_1._0.UserInteraction;
+
+public static class UserInput
+{
+    public static T GetUserInput<T>()
+    {
+        Console.WriteLine("Enter value: ");
+        string input = Console.ReadLine() ?? string.Empty;
+        return (T)Convert.ChangeType(input, typeof(T));
+    }
+}
