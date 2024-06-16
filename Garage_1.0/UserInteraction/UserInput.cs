2073356667372cccf936356319c1fbc057fe2b7b
rb <rkrb@tyfonmail.se>
2024-06-16 18:59:26 +0200

    validation added to user input

--- Garage_1.0/UserInteraction/UserInput.cs
+++ Garage_1.0/UserInteraction/UserInput.cs
@@ -4,9 +4,31 @@ public static class UserInput
 {
     public static T GetUserInput<T>()
     {
-        Console.WriteLine("Enter value: ");
-        var input = Console.ReadLine() ?? string.Empty;
+        object? input;
+        bool isValid;
+        do
+        {
+            Console.WriteLine("Enter value: ");
+            input = Console.ReadLine();
+
+            isValid = ValidateUserInput(input);
+        } while (!isValid);
+
+
         return (T)Convert.ChangeType(input, typeof(T));
+    }
+
+    private static bool ValidateUserInput<T>(T input)
+    {
+        bool result = true;
+        var inputResult = Convert.ChangeType(input, typeof(T));
+        if (inputResult.Equals(string.Empty))
+        {
+            result = false;
+        }
+
+
+        return result;
     }
 
     public static T GetInputEnum<T>() where T : struct, Enum
