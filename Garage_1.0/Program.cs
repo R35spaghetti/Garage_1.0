a7ada1d60d3b6c3837dbf21cd72e413816ceca66
rb <rkrb@tyfonmail.se>
2024-06-16 17:20:44 +0200

    Menu implemented

--- Garage_1.0/Program.cs
+++ Garage_1.0/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello");
+﻿using Garage_1._0.Features;
+using Garage_1._0.Handlers;
+using Garage_1._0.Handlers.Delegates;
+using Garage_1._0.Models;
+using Garage_1._0.UserInteraction.Menu;
+
+Garage<Vehicle> garage = new Garage<Vehicle>(10);
+
+GarageHandler garageHandler = new GarageHandler(garage, new GarageUserFilter(new GarageFilters(garage)));
+
+InteractiveMenu interactiveMenu = new InteractiveMenu(garageHandler);
+interactiveMenu.MainMenu();
