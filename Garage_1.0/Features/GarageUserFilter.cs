16e95ae0f8bef1f786b2f206b71abf6a01158407
RB <rkrb@tyfonmail.se>
2024-06-17 09:39:36 +0200

    refactored each filter into a method

--- Garage_1.0/Features/GarageUserFilter.cs
+++ Garage_1.0/Features/GarageUserFilter.cs
@@ -19,105 +19,116 @@ public class GarageUserFilter
         string fuelType = "";
         string wheels = "";
         string year = "";
+
         if (vehicleTypes == GarageOptions.VehicleTypes.Car)
         {
-            string engineLayout = "";
-
-
-            Dictionary<string, string> userInput = new Dictionary<string, string>
-            {
-                { "Colour", colour },
-                { "FuelType", fuelType },
-                { "Wheels", wheels },
-                { "Year", year },
-                { "EngineLayout", engineLayout }
-            };
-
-
-            foreach (var item in options)
-            {
-                switch (item)
-                {
-                    case '1':
-                        Console.WriteLine("Enter what colour to search");
-                        colour = UserInput.GetUserInput<string>();
-                        break;
-                    case '2':
-                        Console.WriteLine("Enter what fuel type to search");
-                        fuelType = UserInput.GetUserInput<string>();
-                        break;
-                    case '3':
-                        Console.WriteLine("Enter what wheels to search");
-                        wheels = UserInput.GetUserInput<int>().ToString();
-                        break;
-                    case '4':
-                        Console.WriteLine("Enter what year to search");
-                        year = UserInput.GetUserInput<int>().ToString();
-                        break;
-                    case '5':
-                        Console.WriteLine("Enter engine layout to search");
-                        engineLayout = UserInput.GetUserInput<string>();
-                        break;
-                }
-            }
-
-            userInput["Colour"] = colour;
-            userInput["FuelType"] = fuelType;
-            userInput["Wheels"] = wheels;
-            userInput["Year"] = year;
-            userInput["EngineLayout"] = engineLayout;
-            _garageFilters.ApplyVehicleFilters(userInput);
+            GarageFilterCar(colour, fuelType, wheels, year, options);
         }
 
         if (vehicleTypes == GarageOptions.VehicleTypes.Motorcycle)
         {
-            string length = "";
-
-
-            Dictionary<string, string> userInput = new Dictionary<string, string>
-            {
-                { "Colour", colour },
-                { "FuelType", fuelType },
-                { "Wheels", wheels },
-                { "Year", year },
-                { "Length", length }
-            };
-
-
-            foreach (var item in options)
-            {
-                switch (item)
-                {
-                    case '1':
-                        Console.WriteLine("Enter what colour to search");
-                        colour = UserInput.GetUserInput<string>();
-                        break;
-                    case '2':
-                        Console.WriteLine("Enter what fuel type to search");
-                        fuelType = UserInput.GetUserInput<string>();
-                        break;
-                    case '3':
-                        Console.WriteLine("Enter what wheels to search");
-                        wheels = UserInput.GetUserInput<int>().ToString();
-                        break;
-                    case '4':
-                        Console.WriteLine("Enter what year to search");
-                        year = UserInput.GetUserInput<int>().ToString();
-                        break;
-                    case '5':
-                        Console.WriteLine("Enter length to search");
-                        length = UserInput.GetUserInput<int>().ToString();
-                        break;
-                }
-            }
-
-            userInput["Colour"] = colour;
-            userInput["FuelType"] = fuelType;
-            userInput["Wheels"] = wheels;
-            userInput["Year"] = year;
-            userInput["Length"] = length;
-            _garageFilters.ApplyVehicleFilters(userInput);
+            GarageFilterMC(colour, fuelType, wheels, year, options);
         }
+    }
+
+    private void GarageFilterMC(string colour, string fuelType, string wheels, string year, List<char> options)
+    {
+        string length = "";
+
+
+        Dictionary<string, string> userInput = new Dictionary<string, string>
+        {
+            { "Colour", colour },
+            { "FuelType", fuelType },
+            { "Wheels", wheels },
+            { "Year", year },
+            { "Length", length }
+        };
+
+
+        foreach (var item in options)
+        {
+            switch (item)
+            {
+                case '1':
+                    Console.WriteLine("Enter what colour to search");
+                    colour = UserInput.GetUserInput<string>();
+                    break;
+                case '2':
+                    Console.WriteLine("Enter what fuel type to search");
+                    fuelType = UserInput.GetUserInput<string>();
+                    break;
+                case '3':
+                    Console.WriteLine("Enter what wheels to search");
+                    wheels = UserInput.GetUserInput<int>().ToString();
+                    break;
+                case '4':
+                    Console.WriteLine("Enter what year to search");
+                    year = UserInput.GetUserInput<int>().ToString();
+                    break;
+                case '5':
+                    Console.WriteLine("Enter length to search");
+                    length = UserInput.GetUserInput<int>().ToString();
+                    break;
+            }
+        }
+
+        userInput["Colour"] = colour;
+        userInput["FuelType"] = fuelType;
+        userInput["Wheels"] = wheels;
+        userInput["Year"] = year;
+        userInput["Length"] = length;
+        _garageFilters.ApplyVehicleFilters(userInput);
+    }
+
+    private void GarageFilterCar(string colour, string fuelType, string wheels, string year, List<char> options)
+    {
+        string engineLayout = "";
+
+
+        Dictionary<string, string> userInput = new Dictionary<string, string>
+        {
+            { "Colour", colour },
+            { "FuelType", fuelType },
+            { "Wheels", wheels },
+            { "Year", year },
+            { "EngineLayout", engineLayout }
+        };
+
+
+        foreach (var item in options)
+        {
+            switch (item)
+            {
+                case '1':
+                    Console.WriteLine("Enter what colour to search");
+                    colour = UserInput.GetUserInput<string>();
+                    break;
+                case '2':
+                    Console.WriteLine("Enter what fuel type to search");
+                    fuelType = UserInput.GetUserInput<string>();
+                    break;
+                case '3':
+                    Console.WriteLine("Enter what wheels to search");
+                    wheels = UserInput.GetUserInput<int>().ToString();
+                    break;
+                case '4':
+                    Console.WriteLine("Enter what year to search");
+                    year = UserInput.GetUserInput<int>().ToString();
+                    break;
+                case '5':
+                    Console.WriteLine("Enter engine layout to search");
+                    engineLayout = UserInput.GetUserInput<string>();
+                    break;
+            }
+        }
+
+        userInput["Colour"] = colour;
+        userInput["FuelType"] = fuelType;
+        userInput["Wheels"] = wheels;
+        userInput["Year"] = year;
+        userInput["EngineLayout"] = engineLayout;
+        _garageFilters.ApplyVehicleFilters(userInput);
     }
 
     public List<char> IterateThroughOptions(string answer)
