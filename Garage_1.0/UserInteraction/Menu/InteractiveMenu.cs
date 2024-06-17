7e22a29ba8fa6cb177c5adcfa2c45eba0d191765
RB <rkrb@tyfonmail.se>
2024-06-17 18:58:50 +0200

    find numberplate search - toUpper()

--- Garage_1.0/UserInteraction/Menu/InteractiveMenu.cs
+++ Garage_1.0/UserInteraction/Menu/InteractiveMenu.cs
@@ -43,7 +43,7 @@ public class InteractiveMenu
                     _garageHandler.RemoveVehicle(UserInput.GetUserInput<string>());
                     break;
                 case 4:
-                    _garageHandler.FindNumberPlate(UserInput.GetUserInput<string>());
+                    _garageHandler.FindNumberPlate(UserInput.GetUserInput<string>().ToUpper());
                     break;
                 case 5:
                     _garageHandler.FindVehicle();
