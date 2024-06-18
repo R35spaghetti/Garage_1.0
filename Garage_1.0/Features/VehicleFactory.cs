5fa534e0c862f9ebe5efb682964fc1808eb823f8
RB <rkrb@tyfonmail.se>
2024-06-18 11:59:58 +0200

    added the new models to VehicleFactory.cs, now uses a switch case

--- Garage_1.0/Features/VehicleFactory.cs
+++ Garage_1.0/Features/VehicleFactory.cs
@@ -8,7 +8,6 @@ public static class VehicleFactory
 {
     public static T CreateVehicle<T>(GarageOptions.VehicleTypes vehicleType) where T : Vehicle
     {
-
         string? numberPlate;
         do
         {
@@ -17,29 +16,46 @@ public static class VehicleFactory
         } while (!Vehicle.IsNumberPlateUnique(numberPlate));
 
         Console.WriteLine("Enter type of colour");
-        string? colour = UserInput.GetUserInput<string>();
+        string colour = UserInput.GetUserInput<string>();
         Console.WriteLine("What kind of fuel type");
-        string? fuelType = UserInput.GetUserInput<string>();
+        string fuelType = UserInput.GetUserInput<string>();
         Console.WriteLine("Enter amount of wheels ");
         int wheels = UserInput.GetUserInput<int>();
         Console.WriteLine("Enter the year model");
         int year = UserInput.GetUserInput<int>();
-        
-        if (vehicleType == GarageOptions.VehicleTypes.CAR)
-        {
-            Console.WriteLine("What is the engine layout");
-            string? engineLayout = UserInput.GetUserInput<string>();
-            var car = new Car(numberPlate, colour, fuelType, wheels, year, engineLayout);
-            return (T)(Vehicle)car;
-        }
 
-        else if (vehicleType == GarageOptions.VehicleTypes.MOTORCYCLE)
-        {
-            Console.WriteLine("Enter the length");
-            int length = UserInput.GetUserInput<int>();
 
-            var motorcycle = new Motorcycle(numberPlate, colour, fuelType, wheels, year, length);
-            return (T)(Vehicle)motorcycle;
+        switch (vehicleType)
+        {
+            case GarageOptions.VehicleTypes.CAR:
+                Console.WriteLine("What is the engine layout");
+                string engineLayout = UserInput.GetUserInput<string>();
+                var car = new Car(numberPlate, colour, fuelType, wheels, year, engineLayout);
+                return (T)(Vehicle)car;
+
+            case GarageOptions.VehicleTypes.MOTORCYCLE:
+                Console.WriteLine("Enter the length");
+                int length = UserInput.GetUserInput<int>();
+                var motorcycle = new Motorcycle(numberPlate, colour, fuelType, wheels, year, length);
+                return (T)(Vehicle)motorcycle;
+
+            case GarageOptions.VehicleTypes.BOAT:
+                Console.WriteLine("Enter the amount Of engines");
+                int amountOfEngines = UserInput.GetUserInput<int>();
+                var boat = new Boat(numberPlate, colour, fuelType, wheels, year, amountOfEngines);
+                return (T)(Vehicle)boat;
+
+            case GarageOptions.VehicleTypes.BUS:
+                Console.WriteLine("Enter the amount Of seats");
+                int seats = UserInput.GetUserInput<int>();
+                var bus = new Bus(numberPlate, colour, fuelType, wheels, year, seats);
+                return (T)(Vehicle)bus;
+
+            case GarageOptions.VehicleTypes.AIRPLANE:
+                Console.WriteLine("Enter the amount Of wings");
+                int wings = UserInput.GetUserInput<int>();
+                var airplane = new Airplane(numberPlate, colour, fuelType, wheels, year, wings);
+                return (T)(Vehicle)airplane;
         }
 
         throw new ArgumentException($"{typeof(T).Name} is not a recognized vehicle.", nameof(vehicleType));
