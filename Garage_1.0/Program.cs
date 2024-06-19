54cd9d3ce5d539e546985ec02b2baaf7d8c525ea
RB <rkrb@tyfonmail.se>
2024-06-19 09:58:28 +0200

    Program starts by asking the user for garage size

--- Garage_1.0/Program.cs
+++ Garage_1.0/Program.cs
@@ -2,9 +2,11 @@
 using Garage_1._0.Handlers;
 using Garage_1._0.Handlers.Delegates;
 using Garage_1._0.Models;
+using Garage_1._0.UserInteraction;
 using Garage_1._0.UserInteraction.Menu;
 
-Garage<Vehicle> garage = new Garage<Vehicle>(10);
+Console.WriteLine("Enter the size of your garage");
+Garage<Vehicle> garage = new Garage<Vehicle>(UserInput.GetUserInput<int>());
 
 GarageHandler garageHandler = new GarageHandler(garage, new GarageUserFilter(new GarageFilters(garage)));
 
