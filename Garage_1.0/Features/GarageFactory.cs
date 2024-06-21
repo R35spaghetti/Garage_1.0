3064fd2bedf45f2aff7fe0f2a0dc3ed029cfd78c
rb <rkrb@tyfonmail.se>
2024-06-21 23:46:35 +0200

    Creates Garage T

---
+++ Garage_1.0/Features/GarageFactory.cs
@@ -0,0 +1,14 @@
+using Garage_1._0.Models;
+using Garage_1._0.UserInteraction;
+
+namespace Garage_1._0.Features;
+
+public static class GarageFactory
+{
+    public static Garage<T> CreateGarage<T>() where T : Vehicle
+    {
+        Console.WriteLine("Enter the size of the new garage");
+        return new Garage<T>(UserInput.GetUserInput<int>());
+    }
+}
+
