2313932324fe11672ca16fb46eab3768931d605d
rb <rkrb@tyfonmail.se>
2024-06-23 10:18:30 +0200

    made GarageManagerFactory.cs static

--- Garage_1.0/Features/GarageManagerFactory.cs
+++ Garage_1.0/Features/GarageManagerFactory.cs
@@ -5,9 +5,9 @@ using Garage_1._0.Models.Contracts;
 
 namespace Garage_1._0.Features;
 
-public class GarageManagerFactory
+public static class GarageManagerFactory
 {
-    public  GarageManager<T> CreateGarageManager<T>(GarageOptions.VehicleTypes vehicleType) where T : Vehicle,IVehicle
+    public static GarageManager<T> CreateGarageManager<T>(GarageOptions.VehicleTypes vehicleType) where T : Vehicle,IVehicle
     {
         var garageManager = new GarageManager<T>();
         var garage = GarageFactory.CreateGarage<T>();
