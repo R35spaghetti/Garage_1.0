84bea08eb38227ac5321f58a600023d42e5adeff
rb <rkrb@tyfonmail.se>
2024-06-24 09:52:29 +0200

    updated wrapper to be more specific towards Garage<T>

--- Garage_1.0/Features/GenericDictionaryWrapper.cs
+++ Garage_1.0/Features/GenericDictionaryWrapper.cs
@@ -1,19 +1,21 @@
+using Garage_1._0.Models;
+
 namespace Garage_1._0.Features;
 
 public class GenericDictionaryWrapper
 {
-    private Dictionary<Enum, object> _innerDict = new Dictionary<Enum, object>();
+    private Dictionary<Enum, object> _innerDict = new();
 
-    public void Add<T>(Enum key, T value) where T : class
+    public void Add<T>(Enum key, Garage<T> value) where T : Vehicle
     {
         _innerDict.Add(key, value);
     }
 
-    public T Retrieve<T>(Enum key) where T : class
+    public Garage<T> Retrieve<T>(Enum key) where T : Vehicle
     {
         if (_innerDict.TryGetValue(key, out var obj))
         {
-            return obj as T;
+            return obj as Garage<T>;
         }
         return null;
     }
